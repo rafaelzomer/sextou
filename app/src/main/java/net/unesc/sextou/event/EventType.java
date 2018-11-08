456ae1db4e06f9d83579e7d1942b98722510f2c0
Mateus Generoso <mtsgeneroso@gmail.com>
2018-11-08 19:26:40 -0200

    Added click listeners on event screen

--- app/src/main/java/net/unesc/sextou/event/EventType.java
+++ app/src/main/java/net/unesc/sextou/event/EventType.java
@@ -1,16 +1,36 @@
 package net.unesc.sextou.event;
 
 
+import android.view.View;
+
 import java.io.Serializable;
+import java.util.function.Consumer;
 
 public class EventType implements Serializable {
     private String title;
-
     private int icon;
+    private boolean checked;
+    private View.OnClickListener listener;
 
     public EventType(String title, int icon) {
         this.title = title;
         this.icon = icon;
+    }
+
+    public View.OnClickListener getListener() {
+        return listener;
+    }
+
+    public void setListener(View.OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
     }
 
     public String getTitle() {
